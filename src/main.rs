32888862a442046bb60f7bd82ffeb60eee28e511
StormLight14 <stormpower44@protonmail.com>
2022-10-23 12:17:33 -0600

    cargo init
    Made easier to run because project was made with cargo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::io;
+use rand::seq::IteratorRandom;
+
 fn main() {
+    //Create &str full of characters to randomly choose
     let printable = r##"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~ "##;
     let mut password_length = String::new();
-    
+
     println!("Enter the length of password you want generated: ");
+
+    //Get user input for pass length; Change user input to a u32
     io::stdin().read_line(&mut password_length).expect("Failed to read line.");
     let password_length: u32 = password_length.trim().parse().expect("Please enter a number.");
     let mut password = String::new();
 
+    //Go through the password length and
     for _num in 0..password_length {
         let mut rng = rand::thread_rng();
         let rand_char = printable.chars().choose(&mut rng);
@@ -17,3 +24,4 @@ fn main() {
     }
     println!("{}", password);
 }
+
